887859f9ca76adca197e31fb82b7645339d62c9f
Cesar Valdez <cesar@hyperion.local>
2020-09-29 18:44:08 +0200

    Adding search bar

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { FlatList, StyleSheet, Text, View } from 'react-native';
 import {Populate, Persons, T, TT} from './lib/Populate'
+import { SearchBar } from 'react-native-elements';
+
 
 const styles = StyleSheet.create({
     container: {
@@ -27,13 +29,27 @@ const FlatListBasics = () => {
     const [data, setData] = useState( async () => { 
         let data = await fetchData() 
         setData(data)
-    } )
+    })
+
+    let search = ''
+
+    function updateSearch() {
+        console.log('searching...')
+    }
 
     return (
         <View style={styles.container}>
-        <FlatList
+        <SearchBar
+        placeholder="Type Here..."
+        onChangeText={updateSearch}
+        value={search}
+        />
+        <FlatList  
         data = {data}
-        renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+        renderItem = {
+            ({item}) => 
+            <Text style={styles.item}>{item.key}</Text>
+        }
         />
         </View>
     );
